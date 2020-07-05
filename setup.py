be699f35892827ac34eebbdbc2ddca64964db61d
Lednik7 <52196169+Lednik7@users.noreply.github.com>
2020-07-05 17:40:34 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='SpacyToolKit',
+   version='1.0.0',
+   packages=['SpacyToolKit.py'],
+   author='Maxim Gerasimov',
+   author_email='none',
+   install_requires=['spacy>=2.0', 'pymorphy2>=0.8']
+)
