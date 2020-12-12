9a0e45adf1b5d6f55970130b5734e9a3147d05df
Lednik7 <52196169+Lednik7@users.noreply.github.com>
2020-12-12 23:09:53 +0300

    bugs fixed
    Fixed problems with functions, information output

--- model_selection/selection.py
+++ model_selection/selection.py
@@ -4,38 +4,38 @@ import random
 from SpacyToolKit.metrics import evaluate
 
 def cross_val_score(model, data, label=None, n_splits=5, shuffle=False, seed=42):
-  """
-  the function divides the array into pieces returns the average score for each metric
-  """
-  assert len(data) != 1, "Length of test data must be more than 1"
-  assert len(data) >= n_splits >= 2, f"Please, change n_splits argument--> {n_splits}"
-  assert label, "You have to set the test data label"
+    """
+    the function divides the array into pieces returns the average score for each metric
+    """
+    assert len(data) != 1, "Length of test data must be more than 1"
+    assert len(data) >= n_splits >= 2, f"Please, change n_splits argument--> {n_splits}"
+    assert label, "You have to set the test data label"
 
-  random.seed(seed)
-  if shuffle:
-    data = random.shuffle(data)
-  data = [data[i:i+n_splits] for i in range(0, len(data), len(data) // n_splits)]
+    random.seed(seed)
+    if shuffle:
+        data = random.shuffle(data)
+    data = [data[i:i + n_splits] for i in range(0, len(data), len(data) // n_splits)]
 
-  precision = []
-  recall = []
-  f1 = []
+    precision = []
+    recall = []
+    f1 = []
 
-  for fold in data[:n_splits]:
-    scores = evaluate(model, fold)
-    if scores != dict():
-      p, r, f = scores[label].values()
-      precision.append(p)
-      recall.append(r)
-      f1.append(f)
+    for fold in data[:n_splits]:
+        scores = evaluate(model, fold)
+        if scores != dict():
+            p, r, f = scores[label].values()
+            precision.append(p)
+            recall.append(r)
+            f1.append(f)
 
-  return [(label, np.mean(lst)) for label, lst in (("precision", precision), ("recall", recall), ("f1", f1))]
+    return [(label, np.mean(lst)) for label, lst in (("precision", precision), ("recall", recall), ("f1", f1))]
 
 def train_test_val_split(data, test_size=0.3, val_size=0.3, random_state=42):
-  """
-  separator for train, test, validation
-  """
-  assert (test_size + val_size) < 1, f"train_size = 1 - (test_size + val_size). Your train_size --> {1 - (test_size + val_size)}"
-  
-  Train, Test = train_test_split(data, train_size=1-(test_size + val_size), random_state=random_state)
-  Val, Test = train_test_split(Test, test_size=test_size/(test_size + val_size), random_state=random_state) 
-  return Train, Test, Val
+    """
+    separator for train, test, validation
+    """
+    assert (test_size + val_size) < 1, f"train_size = 1 - (test_size + val_size). Your train_size --> {1 - (test_size + val_size)}"
+
+    Train, Test = train_test_split(data, train_size=1-(test_size + val_size), random_state=random_state)
+    Val, Test = train_test_split(Test, test_size=test_size/(test_size + val_size), random_state=random_state)
+    return Train, Test, Val
--- utils/functions.py
+++ utils/functions.py
@@ -1,104 +1,93 @@
-import random
 from SpacyToolKit.Tools import words_count
 from spacy.util import filter_spans
+from collections import Counter
 from tqdm import tqdm
-import re
 from itertools import chain
 from copy import deepcopy
+import random
+import re
 
 class Span():
-  def __init__(self, lst):
-    self.start = lst[0]
-    self.end = lst[1]
-    self.label = lst[2]
+    def __init__(self, lst):
+      self.start = lst[0]
+      self.end = lst[1]
+      self.label = lst[2]
 
-  def get_tuple(self):
-    return self.start, self.end, self.label
+    def get_tuple(self):
+        return self.start, self.end, self.label
 
 def length_count(data):
-  """
-  returns data in the format: {unique length:count}
-  """
-  res = {}
-  for i in data:
-    length = len(i[1]["entities"])
-    if length not in res:
-      res[length] = 1
-    else:
-      res[length] += 1
-  return res
+    return Counter(len(i[1]["entities"]) for i in data).most_common()
 
 def choice_element_length(data, length):
-  return [data[i] for i in range(len(data)) if  len(data[i][1]['entities']) >= length]
+    return [data[i] for i in range(len(data)) if  len(data[i][1]['entities']) >= length]
 
 def remove_empty(data):
-  return [data[i] for i in range(len(data)) if len(data[i][1]['entities']) != 0]
+    return [data[i] for i in range(len(data)) if len(data[i][1]['entities']) != 0]
 
 def flatten(data, coef=0.3, seed=42):
-  """
-  function to straighten an array from an array
-  """
-  random.seed(seed)
-  flat = []
-  for i in data:
-    for j in i:
-      flat.append(j)
-  flat = [i[0] for i in words_count(flat)[::-1]]
-  if coef:
-    flat = random.sample(flat, round(len(flat)*coef))
-  return flat
+    """
+    function to straighten an array from an array
+    """
+    random.seed(seed)
+    flat = [j for i in data for j in i]
+    flat = [i[0] for i in words_count(flat)[::-1]]
+    if coef:
+        flat = random.sample(flat, round(len(flat)*coef))
+    return flat
 
 def unique(x):
-  """
-  returns a unique array value
-  """
-  return tuple(set(x))
+    """
+    returns a unique array value
+    """
+    return tuple(set(x))
 
 def find_position(data, word):
-  """
-  the function finds all occurrences of a word in a text ignoring case
-  """
-  try:
-    return [i.span() for i in re.finditer(r"\b{}\b".format(word), data, flags=re.IGNORECASE)]
-  except:
-    return [(0, 0)]
+    """
+    the function finds all occurrences of a word in a text ignoring case
+    """
+    try:
+      return [i.span() for i in re.finditer(r"{}".format(word), data, flags=re.IGNORECASE)]
+    except:
+      return [(0, 0)]
 
 def to_format(text, data, label):
-  """
-  function formats data to standard Spacy models
-  """
-  res = []
-  for word in data:
-    position = find_position(text, word)
-    for pos_start, pos_end in position:
-      if pos_start + pos_end != 0:
-        res.append(Span([pos_start, pos_end, label]))      
+    """
+    function formats data to standard Spacy models
+    """
+    res = []
+    for word in data:
+        position = find_position(text, word)
+        for pos_start, pos_end in position:
+            if pos_start + pos_end != 0:
+                res.append(Span([pos_start, pos_end, label]))
 
-  return [i.get_tuple() for i in filter_spans(res)]
+    return [i.get_tuple() for i in filter_spans(res)]
 
 def preprocessing(texts, words, label, coef=0.3, all_tasks=False, include_repeat=True, progressbar=True):
-  """
-  the function returns the processed array for the Spacy standard
-  """
-  train = []
-  enit = {}
+    """
+    the function returns the processed array for the Spacy standard
+    """
+    train = []
+    enit = {}
 
-  assert 0 < coef <= 1, f"The argument must be in the range (0 < coef <= 1) --> {coef}"
+    assert 0 < coef <= 1, f"The argument must be in the range (0 < coef <= 1) --> {coef}"
 
-  if all_tasks:
-    words_f = flatten(words, coef)
-    if coef == 1:
-      include_repeat = False
-  else:
-    assert len(texts) == len(words), f"Data must be same length: ({len(texts)}, {len(words)})"
-
-  for i in tqdm(range((len(texts))), disable=not progressbar):
     if all_tasks:
-      if include_repeat:
-        words_f = unique(chain(words_f, words[i]))
-      enit['entities'] = to_format(texts[i], words_f, label)
+        words_f = flatten(words, coef)
+        if coef == 1:
+            include_repeat = False
     else:
-      enit['entities'] = to_format(texts[i], words[i], label)
+      assert len(texts) == len(words), f"Data must be same length: ({len(texts)}, {len(words)})"
+      print("\n\033[31mcoef is ignored because you are using all_tasks=False")
 
-    train.append((texts[i], deepcopy(enit)))
-  return train
+    for i in tqdm(range((len(texts))), disable=not progressbar):
+      if all_tasks:
+        if include_repeat:
+            words_f = unique(chain(words_f, words[i]))
+        enit['entities'] = to_format(texts[i], words_f, label)
+      else:
+        enit['entities'] = to_format(texts[i], words[i], label)
+
+      train.append((texts[i], deepcopy(enit)))
+    return train
