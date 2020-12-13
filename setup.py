48c0dd03fe21625edc2c83ac16bd72aeddf092aa
Lednik7 <52196169+Lednik7@users.noreply.github.com>
2020-12-13 11:18:48 +0300

    UPDATE

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("SpacyToolKit/README.md", 'r') as f:
+    long_description = f.read()
 
 setup(
    name='SpacyToolKit',
-   version='1.0.0',
-   packages=['Tools.py'],
+   version='2.7',
+   description='It is a small library dedicated to the fast Spacy model stack, preparing data for training and training models.',
+   license="MIT",
+   long_description=long_description,
    author='Maxim Gerasimov',
-   install_requires=['spacy>=2.0', 'pymorphy2>=0.8', "googletrans=>3.0.0"]
+   url="https://github.com/Lednik7/SpacyToolKit",
+   packages=find_packages(),
+   install_requires=['spacy>=2.0', 'googletrans>=3.1.0a0', "pymorphy2>=0.8", "sklearn>=0.00.0", "numpy>=1.19.4"],
 )
